3b9407cd807bb6a50004a6118a02e41f0239bb63
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2018-01-04 09:30:24 +0000

    Flake 8

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,9 +1,11 @@
 from data import DICTIONARY, LETTER_SCORES
 
+
 def load_words(filename=DICTIONARY):
     """Load dictionary into a list and return list"""
     with open(filename, 'r') as f:
         return [_.strip() for _ in f.readlines()]
+
 
 def calc_word_value(word):
     """Calculate the value of the word entered into function
@@ -11,12 +13,14 @@ def calc_word_value(word):
     global LETTER_SCORES
     return sum([ LETTER_SCORES.get(c, 0) for c in word.upper() ])
 
+
 def max_word_value(list_of_words=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    l = load_words() if list_of_words is None else list(list_of_words)
-    l.sort(key=calc_word_value, reverse=True)
-    return l[0]
+    words = load_words() if list_of_words is None else list(list_of_words)
+    words.sort(key=calc_word_value, reverse=True)
+    return words[0]
+
 
 if __name__ == "__main__":
-    pass # run unittests to validate
+    pass  # run unittests to validate
